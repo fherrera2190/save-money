5d9ac3fc64e46cd5faf51e3a754b5a72e7d61993
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-11-09 15:56:51 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { CardProduct } from "./components/CardProduct";
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <CardProduct />
+    </>
+  );
+}
+
+export default App;
